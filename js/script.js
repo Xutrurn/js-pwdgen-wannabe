d51f4b8c4c444db101c04d9d0de02414e2a71186
Xutrurn <kanjoskillz@gmail.com>
2020-12-13 11:25:45 +0100

    fix v2

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 var name, surname, color, secretNumb;
 
 name = prompt("Insert your first name");
-surname = prompt("insert your last name");
+surname = prompt("Insert your last name");
 color = prompt("Insert your favorite color");
 secretNumb = 19;
 
@@ -11,4 +11,12 @@ rndpwd = name + surname + color + secretNumb;
 
 // console.log(rndpwd);
 
+// document.getElementById("random-pwd").innerHTML += rndpwd;
+
 document.getElementById("random-pwd").innerHTML += rndpwd;
+
+// rndpwd.length
+
+// console.log(rndpwd.length);
+
+document.getElementById("pwd-length").innerHTML += rndpwd.length;
